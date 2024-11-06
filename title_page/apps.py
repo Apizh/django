5bd8ac3049f74b6d2c2dcddf5f4b02af50685428
Apizh <aptizh@gmail.com>
2024-11-06 21:10:15 +0400

    Add requirements and app.

---
+++ title_page/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TitlePageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'title_page'
---
+++ title_page/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def method(request):
+    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+
+
+def method1(request):
+    return HttpResponse("<h1>Hello, world. This second page</h1>")
