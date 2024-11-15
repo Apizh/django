8efdfd338a0227bcbe27ad3f46eeec264c2e857e
Apizh <aptizh@gmail.com>
2024-11-16 01:01:00 +0400

    Add lists input and test DataBase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 class TestVisitor(LiveServerTestCase):
@@ -16,6 +19,12 @@ class TestVisitor(LiveServerTestCase):
         '''Close browser'''
         self.browser.quit()
 
+    def check_for_row_in_list(self, row_text):
+        '''подтверждение строки в таблице списка'''
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''Test can be started'''
         self.browser.get(self.live_server_url)  # добавлен полный URL
@@ -28,22 +37,29 @@ class TestVisitor(LiveServerTestCase):
 
         # Добавление новой задачи
         input_task = self.browser.find_element(By.ID, 'id_new_task')  # исправлено на By.ID
-
-        self.assertEqual(  # исправлено на assertEqual
-            input_task.get_attribute('placeholder'),
-            'Enter task'
-        )
-
         # Input task in field
         input_task.send_keys('Learn Alembic')
-        time.sleep(10)
         # Нажимаем Enter, чтобы добавить задачу
         input_task.send_keys(Keys.ENTER)
-        time.sleep(2)  # Можно заменить на WebDriverWait, но пока используем sleep для упрощения
+        self.wait_for_row_in_list_table('1: Learn Alembic')
+        input_task = self.browser.find_element(By.ID, 'id_new_task')  # Снова находим поле ввода
+        input_task.send_keys('Alembic')
+        input_task.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('2: Alembic')
+        # time.sleep(2) Можно заменить на WebDriverWait, но пока используем sleep для упрощения
 
-        # Проверка, что задача появилась в таблице
-        table = self.browser.find_element(By.ID, 'id_list_table')  # исправлено на By.ID
-        rows = table.find_elements(By.TAG_NAME, 'tr')  # исправлено на By.TAG_NAME
-        self.assertFalse(any(row.text == 'Learn Alembic' for row in rows))
+    def wait_for_row_in_list_table(self, row_text):
+        """Ожидать строку в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')  # исправлено на By.ID
+                rows = table.find_elements(By.TAG_NAME, 'tr')  # исправлено на By.TAG_NAME
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-        # self.fail('Tests can be ending')  # Эта строка вызывает ошибку, если нужно остановить тест
+    # self.fail('Tests can be ending')  # Эта строка вызывает ошибку, если нужно остановить тест
